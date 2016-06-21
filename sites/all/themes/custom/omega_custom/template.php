b3124d7857e2a8edb07e6bc2f7418dd8679bff3d
dmitry <zrayev@mail.ru>
2016-06-22 00:13:06 +0300

    add exapmples

--- sites/all/themes/custom/omega_custom/template.php
+++ sites/all/themes/custom/omega_custom/template.php
@@ -1,7 +1,7 @@
 <?php
 
-/**
- * @file
- * Template overrides as well as (pre-)process and alter hooks for the
- * Omega custom theme.
- */
+function THEMENAME_preprocess_page(&$variables) {
+  if (!empty($variables['node'])) {
+    $variables['theme_hook_suggestions'][] = 'page__' . $variables['node']->type;
+  }
+}
