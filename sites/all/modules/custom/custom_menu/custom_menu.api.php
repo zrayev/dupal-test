135913e79192dfb1080b87059b502d90fc2e65c5
dmitry <zrayev@mail.ru>
2016-07-07 21:21:02 +0300

    fix homework: add drupal_alter(), add api.php, add module_invoke_all

---
+++ sites/all/modules/custom/custom_menu/custom_menu.api.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * hook custom_menu menu
+ *
+ * @param $route
+ */
+function hook_custom_menu_menu($route) {}
+
+/**
+ * hook custom_menu menu alter
+ *
+ * @param $subitems
+ * @param $route
+ */
+function hook_custom_menu_menu_alter(&$subitems, $route) {}
+
